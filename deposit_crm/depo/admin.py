042cbde48ab9bc66bd4b24f88dcc259828e770f6
Jon Ander Oribe <jonan.oribe@gmail.com>
2022-10-10 11:32:49 +0200

    Base

--- deposit_crm/depo/admin.py
+++ deposit_crm/depo/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from depo.models import Article,Deposit
 # Register your models here.
+
+admin.site.register([Deposit,Article])
--- deposit_crm/depo/models.py
+++ deposit_crm/depo/models.py
@@ -1,3 +1,22 @@
+from xml.parsers.expat import model
 from django.db import models
 
 # Create your models here.
+COLORS = (
+    ('0','White'),
+    ('1','Black'),
+    ('2','Red'),
+    ('3','Blue'),
+    ('4','Yellow'),
+)
+class Deposit(models.Model):
+    code = models.IntegerField()
+    description = models.CharField(max_length=30)
+
+class Article(models.Model):
+    code = models.IntegerField()
+    description = models.CharField(max_length=30)
+    quantity = models.IntegerField()
+    color = models.CharField(max_length = 1, choices=COLORS)
+    deposit = models.ForeignKey(Deposit,null=True, on_delete=models.SET_NULL)
+
